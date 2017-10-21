7888c068dc6bdca66c85844e04eea68e59fd5ba1
Allan Liebold <allan.liebold@gmail.com>
2017-10-21 12:52:40 -0700

    writes tests for youre_a_square module

--- test_youre_a_square.py
+++ test_youre_a_square.py
@@ -1,16 +1,26 @@
-"""Test for You're a square!"""
-
+"""Test for You're a square."""
+import pytest
 import random
 
-test.describe("is_square")
-test.it("should work for some examples")
-test.expect(not is_square(-1), "Negative numbers cannot be square numbers")
-test.expect(not is_square( 3))
-test.expect(    is_square( 4))
-test.expect(    is_square(25))
-test.expect(not is_square(26))
+square_data = [(3, False), (4, True), (12, False), (25, True), (26, False),
+               (49, True), (50, False), (64, True), (70, False), (100, True),
+               (144, True), (200, False), (225, True), (361, True),
+               (400, True), (600, False), (625, True), (-1, False)]
 
-test.it("should work for random square numbers")
-for i in range(1,100):
-        r = random.randint(0, 0xfff0)        
-        test.expect(is_square(r * r), "{number} is a square number".format(number=(r * r)))
+
+@pytest.mark.parametrize('arg,expected', square_data)
+def test_square(arg, expected):
+    """Test is_square function.
+
+    n should return True if it is a perfect square, otherwise return False.
+    """
+    from youre_a_square import is_square
+    assert is_square(arg) == expected
+
+
+def test_random():
+    """Test that random squared numbers return True."""
+    from youre_a_square import is_square
+    for i in range(1, 100):
+        r = random.randint(0, 0xfff0)
+        assert is_square(r * r) is True
