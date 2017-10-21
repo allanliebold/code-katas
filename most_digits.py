6b44d8db09e4d6056a1be4eef71df38e91bcb1be
Allan Liebold <allan.liebold@gmail.com>
2017-10-21 14:51:25 -0700

    writes tests and docstrings for most_digits

--- most_digits.py
+++ most_digits.py
@@ -1,17 +1,18 @@
-"""Kata: Find the most digits
+"""Kata: Find the most digits.
+
 Best Practices by Unnamed and others:
 
 def find_longest(xs):
     return max(xs, key=lambda x: len(str(x)))
 """
 
+
 def find_longest(arr):
+    """Return the number in a list that has the most digits."""
     longest_idx = 0
     for i in range(1, len(arr)):
         current = str(arr[i])
         longest = str(arr[longest_idx])
         if len(current) > len(longest):
             longest_idx = i
-        
     return arr[longest_idx]
-        
--- test_most_digits.py
+++ test_most_digits.py
@@ -1,8 +1,26 @@
 """Test for Most digits."""
+import pytest
 
-Test.describe("Basic tests")
-Test.assert_equals(find_longest([1, 10, 100]), 100)
-Test.assert_equals(find_longest([9000, 8, 800]), 9000)
-Test.assert_equals(find_longest([8, 900, 500]), 900)
-Test.assert_equals(find_longest([3, 40000, 100]), 40000)
-Test.assert_equals(find_longest([1, 200, 100000]), 100000)
+test_data = [([1, 10, 100], 100),
+             ([9000, 8, 800], 9000),
+             ([8, 900, 500], 900),
+             ([3, 40000, 100], 40000),
+             ([1, 200, 100000], 100000),
+             ([324, 125, 5999, 100, 4], 5999),
+             ([1.5, 25, 3.99, 4, 2.8], 3.99),
+             ([0.1, 10], 0.1),
+             (['ABC', 'CBA', 'ABCBA', 'CBABCD', 'DBCA'], 'CBABCD')]
+
+
+@pytest.mark.parametrize('arg,expected', test_data)
+def test_most_digits(arg, expected):
+    """Test most_digits returns the correct number."""
+    from most_digits import find_longest
+    assert find_longest(arg) == expected
+
+
+def test_attribute_error():
+    """Raise a TypeError if a single int is passed."""
+    from most_digits import find_longest
+    with pytest.raises(TypeError):
+        find_longest(1)
