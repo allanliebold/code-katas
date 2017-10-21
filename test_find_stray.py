f6d9255dfeae3985671b42dfed958a4e1cbdc276
Allan Liebold <allan.liebold@gmail.com>
2017-10-21 11:03:44 -0700

    Update test_find_stray.py

--- test_find_stray.py
+++ test_find_stray.py
@@ -1,1 +1,13 @@
+"""Test for Find the stray number."""
 
+test.describe("Example Tests")
+
+tests = [
+    [[1,1,1,1,1,1,2], 2],
+    [[2,3,2,2,2], 3],
+    [[3,2,2,2,2], 3],
+]
+
+
+for inp, exp in tests:
+    test.assert_equals(stray(inp), exp)
