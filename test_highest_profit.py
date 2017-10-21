e45f9518ee8e391e87e125af0cdca52ad6eea026
Allan Liebold <allan.liebold@gmail.com>
2017-10-21 11:05:11 -0700

    Update test_highest_profit.py

--- test_highest_profit.py
+++ test_highest_profit.py
@@ -1,1 +1,15 @@
+"""Test for The highest profit wins!"""
 
+from random import randint, shuffle
+
+def test(lst, res):
+  Test.assert_equals(min_max(lst), res, "tested on " + str(lst));
+
+Test.describe("min_max")
+Test.it("should work for some examples")
+test([1,2,3,4,5], [1,5])
+test([2334454,5], [5, 2334454])
+
+for i in xrange(0,20):
+    r = randint(0,100)
+    test([r], [r,r])
