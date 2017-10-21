da82cfbea49dd8276797ab4927ff6da77837c9f3
Allan Liebold <allan.liebold@gmail.com>
2017-10-21 15:25:00 -0700

    writes tests and docstrings for remove_first_last

--- remove_first_last.py
+++ remove_first_last.py
@@ -1,4 +1,4 @@
-"""Kata: Remove First and Last Character
+"""Kata: Remove First and Last Character.
 
 Best Practices by zebulan and others
 
@@ -6,7 +6,9 @@ def remove_char(s):
     return s[1 : -1]
 """
 
+
 def remove_char(s):
+    """Remove the first and last characters from string s."""
     middle = ''
     for char in range(1, len(s) - 1):
         middle += s[char]
--- test_remove_first_last.py
+++ test_remove_first_last.py
@@ -1,8 +1,27 @@
 """Test for Remove First and Last Character."""
+import pytest
 
-Test.describe("Tests")
-Test.assert_equals(remove_char('eloquent'), 'loquen')
-Test.assert_equals(remove_char('country'), 'ountr')
-Test.assert_equals(remove_char('person'), 'erso')
-Test.assert_equals(remove_char('place'), 'lac')
-Test.assert_equals(remove_char('ok'), '')
+test_data = [('eloquent', 'loquen'),
+             ('country', 'ountr'),
+             ('person', 'erso'),
+             ('place', 'lac'),
+             ('ok', ''),
+             ('coffee', 'offe'),
+             ('orangutan', 'ranguta'),
+             ('ampersand', 'mpersan'),
+             ('12345', '234'),
+             ('one two', 'ne tw')]
+
+
+@pytest.mark.parametrize('arg,expected', test_data)
+def test_remove_first_last(arg, expected):
+    """Test that the remove_char function returns correct string."""
+    from remove_first_last import remove_char
+    assert remove_char(arg) == expected
+
+
+def test_type_error():
+    """Passing a number raises a TypeError."""
+    from remove_first_last import remove_char
+    with pytest.raises(TypeError):
+        remove_char(10)
