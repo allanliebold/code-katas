8f7e4f09372a01c7e3e7c7139fd9a17aaadc60f9
Allan Liebold <allan.liebold@gmail.com>
2018-02-24 20:43:36 -0800

    added code from codewars kata

--- javascript/detectPangrams.js
+++ javascript/detectPangrams.js
@@ -1,1 +1,16 @@
+function isPangram(string){
+  string = string.toLowerCase();
+  console.log(string);
+  alpha = "abcdefghijklmnopqrstuvwxyz";
+  alphaCount = 0;
+  
+  for (var i = 0; i < alpha.length; i++) {
+    if(string.includes(alpha[i])) 
+      alphaCount++;
+  }
 
+  if(alphaCount == 26) 
+    return true;
+    
+  return false;
+}
