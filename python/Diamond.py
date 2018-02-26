2d42f1df2d37b93d329efed8d1dc5970afc30cfa
Allan Liebold <allan.liebold@gmail.com>
2018-02-26 08:43:24 -0800

    Update Diamond.py

--- python/Diamond.py
+++ python/Diamond.py
@@ -1,1 +1,12 @@
+def diamond(n):
+    if n % 2 == 0 or n < 0:
+        return None
+     
+    layer = n
+    expected = ""
+    
+    while layer > 0:
+        expected += " "
+        layer--
 
+    return expected
