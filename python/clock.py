93764b883ab976d9ee33d274f6ccf16d89550e2a
Allan Liebold <allan.liebold@gmail.com>
2018-02-28 10:06:51 -0800

    Update clock.py

--- python/clock.py
+++ python/clock.py
@@ -1,5 +1,15 @@
 // Clocky McClockface - Write a function to calculate the time with only the hour hand and the angle.
 
 def what_time_is_it(angle):
-
-    return '12:00'
+    if angle == 0 or angle == 360:
+        return '12:00';
+        
+    if angle > 0 and angle <= 90:
+        return '03:00';      
+    elif angle > 90 and angle <= 180:
+        return '06:00';
+    elif angle > 180 and angle <= 270:
+        return '09:00';
+    else:
+        return '12:00'
+    
