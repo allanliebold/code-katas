0f266e2a311f2442b9d898f851bad5e523499c65
Allan Liebold <allan.liebold@gmail.com>
2018-03-02 09:29:00 -0800

    Update mirrorClock.py

--- python/mirrorClock.py
+++ python/mirrorClock.py
@@ -2,4 +2,7 @@
 // return a string with the correct 'unmirrored' time. 
 
 def what_is_the_time(time_in_mirror):
-  
+    hour = int(time_in_mirror[0:2])
+    minute = int(time_in_mirror[3:])
+    print(hour)
+    print(minute)
