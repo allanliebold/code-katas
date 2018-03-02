c801c8c6c065d3585fda07065a6c85c82f8e8fa5
Allan Liebold <allan.liebold@gmail.com>
2018-03-02 09:21:15 -0800

    Update mirrorClock.py

--- python/mirrorClock.py
+++ python/mirrorClock.py
@@ -1,3 +1,6 @@
 // Clock in Mirror - Given a string representing the apparent time displayed on an analog clock, 
 // return a string with the correct 'unmirrored' time. 
 
+def what_is_the_time(time_in_mirror) {
+  
+}
