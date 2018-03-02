3daa3eec8d83d2bc05fa5322d09460608076f8a1
Allan Liebold <allan.liebold@gmail.com>
2018-03-02 09:52:00 -0800

    Update mirrorClock.py

--- python/mirrorClock.py
+++ python/mirrorClock.py
@@ -1,6 +1,25 @@
 // Clock in Mirror - Given a string representing the apparent time displayed on an analog clock, 
 // return a string with the correct 'unmirrored' time. 
 
+/* Top solution by Drymonade
+
+def what_is_the_time(time_in_mirror):
+    array = time_in_mirror.split(":")
+    a = int(array[0])
+    b = int(array[1])
+    
+    if a == 11: a = 12
+    elif a == 12: a = 11
+    else: a = 11 - a
+    
+    if b == 0: 
+        b = 0
+        a += 1
+    else: b = 60 - b 
+    return "{:02}:{:02}".format(a, b)
+
+*/
+
 def what_is_the_time(time_in_mirror):
     hour = int(time_in_mirror[0:2])
     minute = int(time_in_mirror[3:])
