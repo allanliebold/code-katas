aa31060eeba4696a448066331ad99389cd7fa0ac
Allan Liebold <allan.liebold@gmail.com>
2018-04-02 10:11:25 -0700

    Update isIntArray.js

--- javascript/isIntArray.js
+++ javascript/isIntArray.js
@@ -1,5 +1,7 @@
 /* Is Integer Array? Return true if array is empty or includes only integers. Otherwise, return false. 
 
+Top solution by jrpeasee, jacobb, sqet, codewars@rueif.com, IGRACH, ramsaylanier, and others
+
 function isIntArray(arr) {
   return Array.isArray(arr) && arr.every(function (x) { return Math.floor(x) === x });
 }
