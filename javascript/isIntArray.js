717cde500096a6ef47334cb83081364a7f0e1045
Allan Liebold <allan.liebold@gmail.com>
2018-04-02 10:09:52 -0700

    Update isIntArray.js

--- javascript/isIntArray.js
+++ javascript/isIntArray.js
@@ -1,3 +1,11 @@
+/*
+
+function isIntArray(arr) {
+  return Array.isArray(arr) && arr.every(function (x) { return Math.floor(x) === x });
+}
+
+*/
+
 function isIntArray(arr) {
   if (arr === []) {
     return true;
