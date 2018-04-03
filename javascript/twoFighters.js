75d82a310e6f97fce7885dc559861685f02165ce
Allan Liebold <allan.liebold@gmail.com>
2018-04-03 09:51:46 -0700

    Update twoFighters.js

--- javascript/twoFighters.js
+++ javascript/twoFighters.js
@@ -1,3 +1,7 @@
+/* Two Fighters, One Winner. Two fighter objects take turns attacking. Each has a name and amount of damage dealt per attack. 
+The declareWinner function accepts the two fighters and the firstAttacker as arguments. Return the name of the winner. 
+
+*/
 
 function declareWinner(fighter1, fighter2, firstAttacker) {
   return "Write your code here";
