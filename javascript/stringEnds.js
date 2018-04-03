967c38dfc3b977de7c3511b122c74d240dc37103
Allan Liebold <allan.liebold@gmail.com>
2018-04-03 09:39:55 -0700

    Update stringEnds.js

--- javascript/stringEnds.js
+++ javascript/stringEnds.js
@@ -1,5 +1,9 @@
 /* String Ends With? Given two arguments, return true if the first argument ends with the second argument. 
 
+function solution(str, ending){
+  return new RegExp(ending+"$", "i").test(str);
+}
+
 */
 
 function solution(str, ending){
