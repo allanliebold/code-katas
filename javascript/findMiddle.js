c6fec6cc228729b54dd2b10ee317c4e9b7d241dd
Allan Liebold <allan.liebold@gmail.com>
2018-04-10 10:51:00 -0700

    Update findMiddle.js

--- javascript/findMiddle.js
+++ javascript/findMiddle.js
@@ -1,5 +1,9 @@
 // Get the Middle Character - Given a string, return the middle character if it is odd, return the middle two characters if it is even. 
 
 function getMiddle(s) {
-  
+  if (s.length % 2 === 0) {
+    return s[s.length / 2 - 1] + s[s.length / 2];  
+  } else {
+    return s[Math.floor(s.length / 2)];
+  }
 }
