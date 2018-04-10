fff8a6e7d8bca779e4a549416be2d08276919301
Allan Liebold <allan.liebold@gmail.com>
2018-04-10 10:56:52 -0700

    Update findMiddle.js

--- javascript/findMiddle.js
+++ javascript/findMiddle.js
@@ -10,10 +10,8 @@ function getMiddle(s) {
   }
 }
 
-// Refactored solution 
+// Refactored solution using ternary
 
 function getMiddle(s) {
-  return (s.length % 2 === 0 ? 
-         s[s.length / 2 - 1] + s[s.length / 2] 
-         : s[Math.floor(s.length / 2)]);
+  return (s.length % 2 === 0 ? s[s.length / 2 - 1] + s[s.length / 2] : s[Math.floor(s.length / 2)]); 
 }
