4a86d38e4f7851324e373886191b97080dae16dc
Allan Liebold <allan.liebold@gmail.com>
2018-04-11 08:24:28 -0700

    Update makeNegative.js

--- javascript/makeNegative.js
+++ javascript/makeNegative.js
@@ -1,5 +1,11 @@
 /* Make Negative. Given a number, return it as negative. It may already be negative. 0 cannot be made negative.
 
+Top solution by colbydaugh and others
+
+function makeNegative(num) {
+  return -Math.abs(num);
+}
+
 */
 
 function makeNegative(num) {
