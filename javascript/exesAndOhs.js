167698d11d25ff14b74f77ea35e965205e8ae253
Allan Liebold <allan.liebold@gmail.com>
2018-04-11 08:49:28 -0700

    Update exesAndOhs.js

--- javascript/exesAndOhs.js
+++ javascript/exesAndOhs.js
@@ -1,1 +1,13 @@
+// Exes and Ohs. Given a string, count the number of x and o characters. Return true if they are equal or false if not. 
+// This function should be case insensitive. 
 
+function XO(str) {
+  var exes = 0; 
+  var ohs = 0;
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    if (char === 'x' || char === 'o') 
+      char === 'x' ? exes += 1 : ohs += 1;
+  }
+  return (exes === ohs ? true : false);
+}
