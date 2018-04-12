90f0c3963a613c4ddf3996fe4ebfe20ca525daad
Allan Liebold <allan.liebold@gmail.com>
2018-04-12 09:07:12 -0700

    Update vowelCount.js

--- javascript/vowelCount.js
+++ javascript/vowelCount.js
@@ -1,3 +1,13 @@
 // Vowel Count - Return the number of vowels in a given string
 
-
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = 'aeiou';
+  
+  for (var i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) 
+      vowelsCount += 1;
+  }
+  
+  return vowelsCount;
+}
