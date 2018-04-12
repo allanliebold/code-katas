51974a07277cbe9b0876983fc165572d7e78e3d1
Allan Liebold <allan.liebold@gmail.com>
2018-04-12 09:10:15 -0700

    Update vowelCount.js

--- javascript/vowelCount.js
+++ javascript/vowelCount.js
@@ -1,6 +1,6 @@
 // Vowel Count - Return the number of vowels (a, e, i, o, u) in a given string
 
-/* Top solution
+/* Top solution by Balkoth and others
   
 function getCount(str) {
   return (str.mathc(/[aeiou]/ig)||[]).length; 
