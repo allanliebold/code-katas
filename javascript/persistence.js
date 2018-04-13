cd8a9dc6b23acee1566bef08805cd7047700a4b7
Allan Liebold <allan.liebold@gmail.com>
2018-04-12 19:12:12 -0700

    Update persistence.js

--- javascript/persistence.js
+++ javascript/persistence.js
@@ -5,3 +5,7 @@ which is the number of times you must multiply the digits in num until you reach
 persistence(39) === 3  because 3*9 = 27, 2*7 = 14, 1*4=4
                        and 4 has only one digit
 */
+
+function persistance(num) {
+    
+}
