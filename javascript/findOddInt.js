c65b3d66c7bd4a5c7ace681b82e59ef6f2ddf1af
Allan Liebold <allan.liebold@gmail.com>
2018-04-13 09:49:45 -0700

    Update findOddInt.js

--- javascript/findOddInt.js
+++ javascript/findOddInt.js
@@ -1,5 +1,20 @@
 // Find the Odd Int - Given an array of integers, return the one that appears an odd number of times. 
 
+/* Top solution by harms280 and others
+
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function(el){
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+  
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
+
+*/
+
 function findOdd(A) {
   A.sort();
   var count = 0;
