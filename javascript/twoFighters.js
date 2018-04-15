6fdf08d982514f62fbb943a6751497beb60b5e7e
Allan Liebold <allan.liebold@gmail.com>
2018-04-14 18:56:57 -0700

    Update twoFighters.js

--- javascript/twoFighters.js
+++ javascript/twoFighters.js
@@ -4,7 +4,9 @@ The declareWinner function accepts the two fighters and the firstAttacker as arg
 */
 
 function declareWinner(fighter1, fighter2, firstAttacker) {
-  while (fighter1.health > 0 && fighter2.health > 0) {
+  var fighting = true; 
+  while (fighting) {
+    
     fighter2.health = fighter2.health -= fighter1.damage;
   }
   return winner;
