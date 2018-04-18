16672a24e019453662e58b1b2948abb75a417105
Allan Liebold <allan.liebold@gmail.com>
2018-04-18 14:38:06 -0700

    Update reverseLetter.js

--- javascript/reverseLetter.js
+++ javascript/reverseLetter.js
@@ -1,5 +1,11 @@
 // Reverse Letter - Given a string, return it reversed and omitting any non-alphabet characters
 
 function reverseLetter(str) {
-  return str;  
+  var reverseStr = '';
+  
+  for (var i = str.length - 1; i > -1; i--) {
+    if (str[i].isAlpha())
+      reverseStr += str[i];
+  }
+  return reverseStr;  
 }
