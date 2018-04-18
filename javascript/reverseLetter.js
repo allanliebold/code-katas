bf72b77a983c46754d41d3c8a6be0a98c0a2fd72
Allan Liebold <allan.liebold@gmail.com>
2018-04-18 14:51:00 -0700

    Update reverseLetter.js

--- javascript/reverseLetter.js
+++ javascript/reverseLetter.js
@@ -1,5 +1,18 @@
 // Reverse Letter - Given a string, return it reversed and omitting any non-alphabet characters
 
+/* Top solutions by Roman93, juansgaitan, and others, 
+
+reverseLetter=(s)=>s.replace(/[^a-z]/gi,'').split('').reverse().join('');
+
+function reverseLetter(str) {
+  return str.split('')
+            .reverse()
+            .filter(val => /[a-zA-Z]/.test(val))
+            .join('');  
+}
+
+*/
+
 function reverseLetter(str) {
   var reverseStr = '';
   
