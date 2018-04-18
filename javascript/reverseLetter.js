8f33cda2e51a21ca759e34b9506a14e276961ef5
Allan Liebold <allan.liebold@gmail.com>
2018-04-18 14:48:28 -0700

    Update reverseLetter.js

--- javascript/reverseLetter.js
+++ javascript/reverseLetter.js
@@ -4,8 +4,9 @@ function reverseLetter(str) {
   var reverseStr = '';
   
   for (var i = str.length - 1; i > -1; i--) {
-    if (str[i].isAlpha())
+    if (str[i].match(/[a-z]/)) {
       reverseStr += str[i];
+    }
   }
   return reverseStr;  
 }
