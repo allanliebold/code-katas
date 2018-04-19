f633214c4d4a47b28513aa8ced561dadf3c82d0e
Allan Liebold <allan.liebold@gmail.com>
2018-04-19 09:19:40 -0700

    Update exclamationsAndQuestions.js

--- javascript/exclamationsAndQuestions.js
+++ javascript/exclamationsAndQuestions.js
@@ -3,3 +3,17 @@ product of the two counts.
 
 */
 
+function product(s){
+  var ex = 0;
+  var qu = 0;
+  
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] === '!') 
+      ex += 1;
+    
+    if (s[i] === '?')
+      qu += 1;
+  }
+  
+  return ex * qu;
+}
