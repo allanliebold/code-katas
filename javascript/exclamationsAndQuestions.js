876abac2a90d07d4bc22ceb6ce14456fb06afef6
Allan Liebold <allan.liebold@gmail.com>
2018-04-19 09:30:34 -0700

    Update exclamationsAndQuestions.js

--- javascript/exclamationsAndQuestions.js
+++ javascript/exclamationsAndQuestions.js
@@ -1,6 +1,14 @@
 /* Exclamation Marks Series #13 - Given a string, count the number of exclamation marks and question marks and return the
 product of the two counts. 
 
+Top solution by narayanswa30663
+
+function product(s){
+  excs = s.split('!').length - 1
+  ques = s.split('?').length - 1
+  return excs * ques
+}
+
 */
 
 function product(s){
