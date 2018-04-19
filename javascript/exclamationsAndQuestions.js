2c74f4c939ffb0cb3b8cabf1627c8ebc53d7094b
Allan Liebold <allan.liebold@gmail.com>
2018-04-19 09:32:44 -0700

    Update exclamationsAndQuestions.js

--- javascript/exclamationsAndQuestions.js
+++ javascript/exclamationsAndQuestions.js
@@ -9,6 +9,18 @@ function product(s){
   return excs * ques
 }
 
+narayanswa30663's explanation: 
+
+So basically if we have a string like "Hello! Hello! Hello! Hello", and we want to count the number of exclamation marks, 
+we would do something like this.
+
+var x = "Hello! Hello! Hello! Hello".split("!")
+// x is now ["Hello", " Hello", " Hello", " Hello"]
+// but see that there are four strings in the array x, and only three exclamation marks in the original string
+
+var exclamations = x.length - 1
+// exclamations = 4 - 1 = 3
+
 */
 
 function product(s){
