c76ca5c5aeae75ddd106dc3cd3eca516a3415d79
Allan Liebold <allan.liebold@gmail.com>
2018-04-20 12:44:20 -0700

    Update replaceWithAlph.js

--- javascript/replaceWithAlph.js
+++ javascript/replaceWithAlph.js
@@ -4,11 +4,11 @@ Ignore non-alphabet characters.
 
 function alphabetPosition(text) {
   var alph = 'abcdefghijklmnopqrstuvwxyz';
-  var num = '';
+  var num = [];
   for (var i = 0; i < text.length; i++) {
     if(alph.includes(text[i].toLowerCase())) {
-      num += (alph.indexOf(text[i].toLowerCase()) + 1) + ' ';
+      num.push(alph.indexOf(text[i].toLowerCase()) + 1);
     }
   }
-  return num;
+  return num.join(' ');
 }
