89442ceaa23674e707eedd486732fa4d43ba7791
Allan Liebold <allan.liebold@gmail.com>
2018-04-20 09:43:03 -0700

    Update twoToOne.js

--- javascript/twoToOne.js
+++ javascript/twoToOne.js
@@ -1,4 +1,6 @@
-// Two to One - Take two strings and return a new string that combines them, sorted, with no repeated characters. 
+/* Two to One - Take two strings and return a new string that combines them, sorted, with no repeated characters. 
+
+*/
 
 function longest(s1, s2) {
   var combined = (s1 + s2).split('').sort();
