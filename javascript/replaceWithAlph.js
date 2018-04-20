28f7c5af7b484a9923de587c14c89fad2da48c6c
Allan Liebold <allan.liebold@gmail.com>
2018-04-20 12:41:58 -0700

    Update replaceWithAlph.js

--- javascript/replaceWithAlph.js
+++ javascript/replaceWithAlph.js
@@ -2,3 +2,13 @@
 Ignore non-alphabet characters. 
 */
 
+function alphabetPosition(text) {
+  var alph = 'abcdefghijklmnopqrstuvwxyz';
+  var num = '';
+  for (var i = 0; i < text.length; i++) {
+    if(alph.includes(text[i].toLowerCase())) {
+      num += (alph.indexOf(text[i].toLowerCase()) + 1) + ' ';
+    }
+  }
+  return num;
+}
