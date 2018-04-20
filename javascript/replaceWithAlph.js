33943474055e15eaccad3a8fe26f00f779bc735b
Allan Liebold <allan.liebold@gmail.com>
2018-04-20 12:47:29 -0700

    Update replaceWithAlph.js

--- javascript/replaceWithAlph.js
+++ javascript/replaceWithAlph.js
@@ -1,5 +1,12 @@
 /* Replace with Alphabet Position - Given a string, return a string with each letter replaced by its position in the alphabet.
 Ignore non-alphabet characters. 
+
+Top solution by jimmy-ringo:
+
+const alphabetPosition = (text) => text.replace(/[^a-zA-Z]/g,'')
+                                       .split('').map(e => e.charCodeAt(0) - (e === e.toLowerCase() ? 96 : 64))
+                                       .join(' ');
+
 */
 
 function alphabetPosition(text) {
