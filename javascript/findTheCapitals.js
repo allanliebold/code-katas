3adcb75d9d09613e76ae425d1d447a9bf2d0ac8d
Allan Liebold <allan.liebold@gmail.com>
2018-04-23 13:46:54 -0700

    Update findTheCapitals.js

--- javascript/findTheCapitals.js
+++ javascript/findTheCapitals.js
@@ -1,5 +1,12 @@
 /* Find the Capitals - Given a string 'word' return an array with the indices of any capital letters
 
+Top solution by jgunchenko: 
+var capitals = function (word) {
+  return word.split('').reduce(function(memo, v, i) {
+    return v === v.toUpperCase() ? memo.concat(i) : memo;
+  }, []);
+};
+
 */
 
 var capitals = function (word) {  
