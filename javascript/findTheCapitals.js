bdc05536e55f1aba76adef74c8d3cabfcab2bd9f
Allan Liebold <allan.liebold@gmail.com>
2018-04-23 13:37:19 -0700

    Update findTheCapitals.js

--- javascript/findTheCapitals.js
+++ javascript/findTheCapitals.js
@@ -1,3 +1,13 @@
 /* Find the Capitals - Given a string 'word' return an array with the indices of any capital letters
 
 */
+
+var capitals = function (word) {  
+  var capArr = [];
+  for (var i = 0; i < word.length; i++ ) {
+    if(word[i].match(/[A-Z]/g))
+      capArr.push(i);
+  };
+  
+  return capArr;
+};
