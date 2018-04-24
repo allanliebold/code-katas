44a328936a1567df13ae68540f7037cbcd4ac93b
Allan Liebold <allan.liebold@gmail.com>
2018-04-24 12:57:15 -0700

    Update listNames.js

--- javascript/listNames.js
+++ javascript/listNames.js
@@ -1,1 +1,20 @@
+/* List Names - Given a hashed array of names, return the names in a string with the following format: Name1, Name2 & Name3
 
+*/
+
+function list(names){
+  var listStr = '';
+  
+  if(names.length > 0)
+    listStr += names[0].name;
+  
+  if(names.length > 1) {
+    for(var i = 1; i < names.length - 1; i++) {
+      listStr += ', ' + names[i].name;
+    }
+    
+    listStr += ' & ' + names[names.length - 1].name;
+  }
+  
+  return listStr;
+}
