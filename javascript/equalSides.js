cca8fd35fc2d7b40b85072e26731bae012374144
Allan Liebold <allan.liebold@gmail.com>
2018-04-26 11:07:02 -0700

    Update equalSides.js

--- javascript/equalSides.js
+++ javascript/equalSides.js
@@ -1,3 +1,4 @@
-/* Equal Sides of an Array - 
+/* Equal Sides of an Array - Given an array of integers, return the index where the sum of every integer to the left is equal
+to the sum of every integer to the right. Return -1 if this is not possible. 
 
 */
