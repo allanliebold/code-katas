296c1741b569eb67db459cd55f657b2dab42684c
Allan Liebold <allan.liebold@gmail.com>
2018-04-26 12:38:59 -0700

    Update equalSides.js

--- javascript/equalSides.js
+++ javascript/equalSides.js
@@ -3,13 +3,19 @@ to the sum of every integer to the right. Return -1 if this is not possible.
 
 */
 
+function getSum(sum, num) {
+  return sum += num;  
+}
+
 function findEvenIndex(arr) {
   
   if (arr.length > 2) {
     for (var i = 0; i < arr.length; i++) {
       left = arr.slice(0, i);
       right = arr.slice(i+1);
-      console.log('left:' + left + ' right:' + right);
+      
+      if (left.reduce(getSum, 0) === right.reduce(getSum, 0))
+        return i;
     }
   }
   
