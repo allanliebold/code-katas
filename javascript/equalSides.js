5bba39814777209bf5bcf280d1a0aa587b164493
Allan Liebold <allan.liebold@gmail.com>
2018-04-26 12:42:50 -0700

    Update equalSides.js

--- javascript/equalSides.js
+++ javascript/equalSides.js
@@ -1,6 +1,18 @@
 /* Equal Sides of an Array - Given an array of integers, return the index where the sum of every integer to the left is equal
 to the sum of every integer to the right. Return -1 if this is not possible. 
 
+Top Solution by okedoke80:
+
+function findEvenIndex(arr)
+{
+  for(var i=1; i<arr.length-1; i++) {
+    if(arr.slice(0, i).reduce((a, b) =>  a+b) === arr.slice(i+1).reduce((a, b) =>  a+b)) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 */
 
 function getSum(sum, num) {
