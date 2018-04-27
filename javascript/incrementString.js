0424e5896f01fb29dbecf2a969e163450c651611
Allan Liebold <allan.liebold@gmail.com>
2018-04-27 09:03:14 -0700

    Update incrementString.js

--- javascript/incrementString.js
+++ javascript/incrementString.js
@@ -1,3 +1,21 @@
 /* Increment String
 
 */
+
+function incrementString (strng) {
+  var begin = ''; 
+  var end = '';
+
+  for(var i = 0; i < strng.length; i++) {
+    if(isNaN(strng[i])) {
+      begin += strng[i];
+    } else {
+      end = strng.slice(i, strng.length);
+    }
+  }
+  
+  end = (parseInt(end) + 1).toString();
+  console.log('begin: ' + begin + ' end: ' + end);
+  
+  return begin + end;
+}
