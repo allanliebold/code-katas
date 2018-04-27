9f62e9b3fe63fb45d7987b0dd69e69a74d3e1198
Allan Liebold <allan.liebold@gmail.com>
2018-04-27 10:23:08 -0700

    Update incrementString.js

--- javascript/incrementString.js
+++ javascript/incrementString.js
@@ -2,6 +2,24 @@
 If the string already ends with a number, the number should be incremented by 1. 
 If the string does not end with a number the number 1 should be appended to the new string.
 
+Top Solution by Azuaron:
+
+function incrementString(input) {
+  if(isNaN(parseInt(input[input.length - 1]))) return input + '1';
+  return input.replace(/(0*)([0-9]+$)/, function(match, p1, p2) {
+    var up = parseInt(p2) + 1;
+    return up.toString().length > p2.length ? p1.slice(0, -1) + up : p1 + up;
+  });
+}
+
+Solution by Unnamed:
+
+function incrementString(input) {
+  return input.replace(/([0-8]?)(9*)$/, function(s, d, ns) {
+      return +d + 1 + ns.replace(/9/g, '0');
+    });
+}
+
 */
 
 function incrementString (strng) {
