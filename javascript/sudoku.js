0a47f5c6112099236b36d7e5b57cee06e298b595
Allan Liebold <allan.liebold@gmail.com>
2018-05-02 08:31:33 -0700

    Update sudoku.js

--- javascript/sudoku.js
+++ javascript/sudoku.js
@@ -11,5 +11,16 @@ function doneOrNot(board){
     }
   }
   
+  for (var j = 0; j < board.length; j++) {
+    var col = '';
+    for (var k = 0; k < board.length; k++) {
+      col += board[j][k];
+    }
+    
+    if(col.split('').sort().join('') !== '123456789') {
+      return 'Try again!';
+    }
+  }
+  
   return 'Finished!';
 }
