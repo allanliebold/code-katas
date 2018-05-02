1069b72898e48afba775cd30a6c94469b7d43e44
Allan Liebold <allan.liebold@gmail.com>
2018-05-02 13:35:24 -0700

    Update scramblies.js

--- javascript/scramblies.js
+++ javascript/scramblies.js
@@ -1,6 +1,20 @@
 /* Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match 
 str2, otherwise returns false.
 
+Top Solution by niralpokal:
+
+function scramble(str1, str2) {
+  var map={};
+  for(var i in str1) {
+    map[str1[i]] ? map[str1[i]]++ : map[str1[i]]=1;
+  }
+  for(var i in str2) {
+    if(!map[str2[i]]) return false;
+    map[str2[i]]--;
+  }
+  return true;
+}
+
 */
 
 function scramble(str1, str2) {
@@ -16,3 +30,4 @@ function scramble(str1, str2) {
   
   return true;
 }
+// This solution passes tests, but times out running the full test suite. 
