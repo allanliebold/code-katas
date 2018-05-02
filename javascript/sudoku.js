dc70a2f090b9aa026f518af3be330562f35e4cb7
Allan Liebold <allan.liebold@gmail.com>
2018-05-02 08:37:09 -0700

    Update sudoku.js

--- javascript/sudoku.js
+++ javascript/sudoku.js
@@ -1,6 +1,34 @@
 /* Write a function done_or_not/DoneOrNot passing a board (list[list_lines]) as parameter. If the board is valid return 
 'Finished!', otherwise return 'Try again!'
 
+Top Solution by cave.on:
+
+function doneOrNot(rows){
+
+  var columns = []
+  ,    blocks = [];
+  
+  for (var i = 0; i < 9; i++) {
+    columns[i] = [];
+    
+    for (var j = 0; j < 9; j++) {
+      var k = Math.floor(i / 3) + Math.floor(j / 3) * 3;
+      blocks[k] = blocks[k] || [];
+      
+      blocks[k].push(rows[i][j]);
+      columns[i].push(rows[j][i]);
+    }
+  }
+  
+  var is_valid_row = (row) => row.slice(0).sort((a, b) => a - b).join('') == '123456789';
+  
+  var is_valid = rows.every(is_valid_row) 
+    && columns.every(is_valid_row) 
+    && blocks.every(is_valid_row);
+  
+  return is_valid ? 'Finished!' : 'Try again!';
+}
+
 */
 
 function doneOrNot(board){
