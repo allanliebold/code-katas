2b856e2c180db037d91d4c2266fd238959ebf8a0
Allan Liebold <allan.liebold@gmail.com>
2018-05-02 08:06:55 -0700

    Update sudoku.js

--- javascript/sudoku.js
+++ javascript/sudoku.js
@@ -2,3 +2,14 @@
 'Finished!', otherwise return 'Try again!'
 
 */
+
+function doneOrNot(board){
+  for (var i = 0; i < board.length; i++) {
+    console.log(board[i].sort());
+    if (board[i].sort() !== [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]) {
+      return 'Try again!';
+    }
+  }
+  
+  return 'Finished!';
+}
