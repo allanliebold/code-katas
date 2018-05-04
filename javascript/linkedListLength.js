b72399a40ce26716505b47df6a4f56ce20914d90
Allan Liebold <allan.liebold@gmail.com>
2018-05-04 08:37:14 -0700

    Update linkedListLength.js

--- javascript/linkedListLength.js
+++ javascript/linkedListLength.js
@@ -1,1 +1,13 @@
 
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function length(head) {
+  // Your code goes here.
+}
+
+function count(head, data) {
+  // Your code goes here.
+}
