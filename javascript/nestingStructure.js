1f3daf1fed00f84e5d2c776cf88726c427a1d1bd
Allan Liebold <allan.liebold@gmail.com>
2018-05-04 08:33:32 -0700

    Update nestingStructure.js

--- javascript/nestingStructure.js
+++ javascript/nestingStructure.js
@@ -1,3 +1,8 @@
+/* Nesting Structure Comparison - Complete the function/method to return true/True when its argument is an array that has 
+the same nesting structure as the first array.
+
+*/
+
 
 Array.prototype.sameStructureAs = function (other) {
     // Return 'true' if and only if 'other' has the same
