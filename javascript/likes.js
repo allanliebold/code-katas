b33f8c9beffeb6dd86f42a904e899ab8caf1285a
Allan Liebold <allan.liebold@gmail.com>
2018-05-07 08:42:45 -0700

    Update likes.js

--- javascript/likes.js
+++ javascript/likes.js
@@ -13,4 +13,18 @@ For 4 or more names, the number in and 2 others simply increases.
 function likes(names) {
   if (names.length === 0)
     return 'no one likes this'
+    
+  if (names.length === 1)
+    return `${names[0]} likes this`;
+  
+  if (names.length === 2)
+    return `${names[0]} and ${names[1]} like this`;
+  
+  var str = '';
+  
+  for (var i = 0; i < names.length - 2; i++) {
+    str += `${names[i]}, `;  
+  }
+  
+  return `${str} ${names[i + 1]} and ${names[i + 2]} like this`;
 }
