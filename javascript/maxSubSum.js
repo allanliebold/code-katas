8e60ac5175c54133d06dd4938d133c605edd3314
Allan Liebold <allan.liebold@gmail.com>
2018-05-07 08:01:13 -0700

    Update maxSubSum.js

--- javascript/maxSubSum.js
+++ javascript/maxSubSum.js
@@ -4,17 +4,17 @@ array or list of integers. If the array is made up of only negative numbers, ret
 */
 
 var maxSequence = function(arr){
-  var max = 0; 
-  
+  var max = 0;
+
   if (arr.length === 0) {
     return max; 
   }
   
   for (var i = 0; i < arr.length; i++) {
-    if (max + arr[i] > max) {
-      max = max + arr[i];  
+    if (max + arr[i] > 0) {
+      max = max + arr[i];
     }
   }
   
-  return max;
+  return max; 
 }
