b6b16e63c49ec25f5ae70194b2b7f444a2fd448a
Allan Liebold <allan.liebold@gmail.com>
2018-05-07 08:08:36 -0700

    Update likes.js

--- javascript/likes.js
+++ javascript/likes.js
@@ -1,1 +1,11 @@
+/* Who Likes It? Implement a function likes :: [String] -> String, which must take in input array, 
+containing the names of people who like an item. It must return the display text as shown in the examples:
 
+likes [] // must be "no one likes this"
+likes ["Peter"] // must be "Peter likes this"
+likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+For 4 or more names, the number in and 2 others simply increases.
+
+*/
