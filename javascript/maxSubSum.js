8e4f19fbdb354029e35f1ed63e6b320c7e79c494
Allan Liebold <allan.liebold@gmail.com>
2018-05-07 07:54:37 -0700

    Update maxSubSum.js

--- javascript/maxSubSum.js
+++ javascript/maxSubSum.js
@@ -1,3 +1,4 @@
-/* Maximum Subarray Sum
+/* Maximum Subarray Sum - The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an 
+array or list of integers. If the array is made up of only negative numbers, return 0. 
 
 */
