43854f8943e7962474e9d2aebc2b51db5b4f6c7f
Allan Liebold <allan.liebold@gmail.com>
2018-05-07 08:02:44 -0700

    Update maxSubSum.js

--- javascript/maxSubSum.js
+++ javascript/maxSubSum.js
@@ -1,6 +1,17 @@
 /* Maximum Subarray Sum - The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an 
 array or list of integers. If the array is made up of only negative numbers, return 0. 
 
+Top Solution by itsPG and others:
+
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
 */
 
 var maxSequence = function(arr){
