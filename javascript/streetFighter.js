3c5d06b5cc666f63ae4e8f40b0d890d3a8ab2e27
Allan Liebold <allan.liebold@gmail.com>
2018-05-18 09:06:26 -0700

    Update streetFighter.js

--- javascript/streetFighter.js
+++ javascript/streetFighter.js
@@ -1,5 +1,12 @@
 /* Street Fighter II Character Select - You'll have to simulate the video game's character selection screen behaviour.
+Input -
+the list of game characters in a 2x6 grid;
+the initial position of the selection cursor (top-left is (0,0));
+a list of moves of the selection cursor (which are up, down, left, right);
 
+Output -
+the list of characters who have been hovered by the selection cursor after all the moves 
+(ordered and with repetition, all the ones after a move, wether successful or not, see tests);
 */
 
 
