82b38fb0799a016f1b0b60e3e986f00a345e83d9
Allan Liebold <allan.liebold@gmail.com>
2018-05-18 06:17:29 -0700

    Update arrayDiff.js

--- javascript/arrayDiff.js
+++ javascript/arrayDiff.js
@@ -1,4 +1,6 @@
 /* Array.diff - Implement a difference function. Given arrays a and b, remove any elements from a that are in b and return a. 
 */
 
-
+function array_diff(a, b) {
+  return a.filter(num => !b.includes(num));
+}
