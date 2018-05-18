69ef4ad74cfff6af63859f7ec07bfdf01547ff45
Allan Liebold <allan.liebold@gmail.com>
2018-05-18 06:20:21 -0700

    Update arrayDiff.js

--- javascript/arrayDiff.js
+++ javascript/arrayDiff.js
@@ -5,6 +5,8 @@ Top solution by yrara and others:
 function array_diff(a, b) {
   return a.filter(function(x) { return b.indexOf(x) == -1; });
 }
+
+I actually managed to come up with one of the other top solutions on my own though. 
 */
 
 function array_diff(a, b) {
