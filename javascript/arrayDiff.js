ebd27254fd95fa5a7b2e25cb363c69b24aa86cdd
Allan Liebold <allan.liebold@gmail.com>
2018-05-18 06:19:13 -0700

    Update arrayDiff.js

--- javascript/arrayDiff.js
+++ javascript/arrayDiff.js
@@ -1,4 +1,10 @@
 /* Array.diff - Implement a difference function. Given arrays a and b, remove any elements from a that are in b and return a. 
+
+Top solution by yrara and others:
+
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
 */
 
 function array_diff(a, b) {
