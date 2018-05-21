fde189a79ccae1e00d16b3e5ad86529539387992
Allan Liebold <allan.liebold@gmail.com>
2018-05-21 09:14:19 -0700

    Update expandedNumber.js

--- javascript/expandedNumber.js
+++ javascript/expandedNumber.js
@@ -7,3 +7,12 @@ expandedForm(42); // Should return '40 + 2'
 expandedForm(70304); // Should return '70000 + 300 + 4'
 
 */
+
+function expandedForm(num) {
+  var numStr = num.toString();
+  var finalStr = '';
+  
+  for(var i = 0; i < numStr.length; i++) {
+    var zeroes = numStr.length - (i + 1);
+  }
+}
