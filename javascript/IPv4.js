e4f9c859815caeadbd80f0e85f963fd4a9082e38
Allan Liebold <allan.liebold@gmail.com>
2018-05-21 10:40:50 -0700

    Update IPv4.js

--- javascript/IPv4.js
+++ javascript/IPv4.js
@@ -1,1 +1,7 @@
+/* Is it IPv4 Address? Basic Regex - 
+Implement String#ipv4_address?, which should return true if given object is an IPv4 address - four numbers (0-255) 
+separated by dots.
+
+It should only accept addresses in canonical representation, so no leading 0s, spaces etc.
+*/
 
