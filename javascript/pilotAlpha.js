bd0f8a7f4ba7bbf611f98ddfb229608386105f6e
Allan Liebold <allan.liebold@gmail.com>
2018-05-24 08:12:03 -0700

    Update pilotAlpha.js

--- javascript/pilotAlpha.js
+++ javascript/pilotAlpha.js
@@ -1,5 +1,11 @@
 /* If you can read this...
 Create a function to translate a string to Pilot's alphabet (NATO phonetic alphabet)
 
+Alfa, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, 
+Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, 
+Sierra, Tango, Uniform, Victor, Whiskey, X-ray, Yankee, Zulu.
 
+Input: Hello
+Output: Hotel Echo Lima Lima Oscar
 */
+
