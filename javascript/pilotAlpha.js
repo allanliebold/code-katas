08311abc673d69b57e554329022740868aa697a4
Allan Liebold <allan.liebold@gmail.com>
2018-05-24 10:46:02 -0700

    Update pilotAlpha.js

--- javascript/pilotAlpha.js
+++ javascript/pilotAlpha.js
@@ -10,11 +10,18 @@ Output: Hotel Echo Lima Lima Oscar
 */
 
 function to_nato(words) {
+  const natoAlpha = {'a': 'Alfa', 'b': 'Bravo', 'c': 'Charlie', 'd': 'Delta', 
+    'e': 'Echo', 'f': 'Foxtrot', 'g': 'Golf', 'h': 'Hotel', 'i': 'India', 
+    'j': 'Juliett', 'k': 'Kilo', 'l': 'Lima', 'm': 'Mike', 'n': 'November', 
+    'o': 'Oscar', 'p': 'Papa', 'q': 'Quebec', 'r': 'Romeo', 's':  'Sierra', 
+    't': 'Tango', 'u': 'Uniform', 'v': 'Victor', 'w': 'Whiskey', 'x': 'Xray', 
+    'y': 'Yankee', 'z': 'Zulu'};
+  var words = words.split(' ').join('');
   var nato = words.split('').map(char => {
     if('?!.'.includes(char))
       return char;
-    return char;
+    return natoAlpha[char.toLowerCase()];
+    ;
   });
-  
-  console.log(nato);
+  return nato.join(' ');
 }
