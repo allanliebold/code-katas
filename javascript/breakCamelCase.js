48d6a17a80e7b1f0c19dffda9fd6a61335e6c571
Allan Liebold <allan.liebold@gmail.com>
2018-05-25 10:49:05 -0700

    Update breakCamelCase.js

--- javascript/breakCamelCase.js
+++ javascript/breakCamelCase.js
@@ -5,6 +5,8 @@ function solution(string) {
   return(string.replace(/([A-Z])/g, ' $1'));
 
 }
+
+I went overboard on this one. Replace is a much simpler solution. 
 */ 
 
 let solution = string => string.split('').map(char => char.match(/[A-Z]/) ? ' ' + char : char).join('');
