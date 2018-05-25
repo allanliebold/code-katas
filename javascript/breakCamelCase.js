3fadd9362a882cc28dab871bcac699a2b70739c9
Allan Liebold <allan.liebold@gmail.com>
2018-05-25 10:39:55 -0700

    Update breakCamelCase.js

--- javascript/breakCamelCase.js
+++ javascript/breakCamelCase.js
@@ -1,5 +1,10 @@
 /* Break Camel Case - Make a function that will break up camel casing, adding a space between words. 
 
+Top solution - 
+function solution(string) {
+  return(string.replace(/([A-Z])/g, ' $1'));
+
+}
 */ 
 
 let solution = string => string.split('').map(char => char.match(/[A-Z]/) ? ' ' + char : char).join('');
