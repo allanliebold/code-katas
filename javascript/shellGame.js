226e281fff39a450c7bc4383f1c18910480c931c
Allan Liebold <allan.liebold@gmail.com>
2018-05-26 21:27:11 -0700

    Update shellGame.js

--- javascript/shellGame.js
+++ javascript/shellGame.js
@@ -1,1 +1,12 @@
+/* Shell Game - 
+Given the shell that the ball starts under, and list of swaps, 
+return the location of the ball at the end. All shells are indexed by the position they are in at the time.
+
+For example, given the starting position 0 and the swap sequence [(0, 1), (1, 2), (1, 0)]:
+
+The first swap moves the ball from 0 to 1
+The second swap moves the ball from 1 to 2
+The final swap doesn't affect the position of the ball.
+
+*/
 
