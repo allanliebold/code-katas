68f832dbd314842d3046cd8d8e5a45f8c33c353d
Allan Liebold <allan.liebold@gmail.com>
2018-05-27 21:25:59 -0700

    Update arrayDeepCount.js

--- javascript/arrayDeepCount.js
+++ javascript/arrayDeepCount.js
@@ -1,1 +1,6 @@
+/* Array Deep Count - Array.prototype.length will give you the number of top-level elements in an array.
 
+Your task is to create a function deepCount that returns the number of ALL elements within an array, 
+including any within inner-level arrays.
+
+*/
