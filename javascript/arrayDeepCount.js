a6af2a905becc338ae05209a3c165ab237a84ecc
Allan Liebold <allan.liebold@gmail.com>
2018-05-27 22:07:25 -0700

    Update arrayDeepCount.js

--- javascript/arrayDeepCount.js
+++ javascript/arrayDeepCount.js
@@ -3,6 +3,14 @@
 Your task is to create a function deepCount that returns the number of ALL elements within an array, 
 including any within inner-level arrays.
 
+For example:
+
+deepCount([1, 2, 3]);  
+//>>>>> 3
+deepCount(["x", "y", ["z"]]);  
+//>>>>> 4
+deepCount([1, 2, [3, 4, [5]]]);  
+//>>>>> 7
 */
 
 function deepCount(a){
