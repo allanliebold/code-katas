4566be58ebcb3872ea3ca5367859ef3239f9ed4c
Allan Liebold <allan.liebold@gmail.com>
2018-05-27 17:32:40 -0700

    Update rot13.js

--- javascript/rot13.js
+++ javascript/rot13.js
@@ -1,3 +1,7 @@
 /* ROT13 - ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet.
 
 */
+
+function rot13(message){
+  //your code here
+}
