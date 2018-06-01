8600cee96ddff34ca673b0625dea63ba83621107
Allan Liebold <allan.liebold@gmail.com>
2018-06-01 06:15:04 -0700

    Update superStreetFighter.js

--- javascript/superStreetFighter.js
+++ javascript/superStreetFighter.js
@@ -1,4 +1,4 @@
-/* Super Street Fighter 2 Character Selection - 
+/* Super Street Fighter 2 Character Selection - Same as before but now with the updated character roster. 
 
 fighters = [
 	[       "",    "Ryu",  "E.Honda",  "Blanka",   "Guile", ""       ],
@@ -6,7 +6,7 @@ fighters = [
 	[   "Vega", "T.Hawk", "Fei Long",  "Deejay",   "Cammy", "M.Bison"]
 ];
 
-
+Moving up from Balrog or Sagat should behave the same way as moving up from any of the characters on the top row.
 */
 
 function superStreetFighterSelection(fighters, position, moves){
