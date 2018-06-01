131a8fe6d381e0c3b4e0e449e1c227fa36e00138
Allan Liebold <allan.liebold@gmail.com>
2018-06-01 06:15:41 -0700

    Update superStreetFighter.js

--- javascript/superStreetFighter.js
+++ javascript/superStreetFighter.js
@@ -12,3 +12,23 @@ Moving up from Balrog or Sagat should behave the same way as moving up from any 
 function superStreetFighterSelection(fighters, position, moves){
   return [];
 }
+
+function streetFighterSelection(fighters, position, moves){
+  return moves.map(move => {
+    switch(move) {
+      case 'up':
+        position[0] = 0;
+        break;
+      case 'down':
+        position[0] = 1;
+        break;
+      case 'left':
+        position[1] === 0 ? position[1] = 5 : position[1]--;
+        break;
+      case 'right':
+        position[1] === 5 ? position[1] = 0 : position[1]++;
+        break;
+    }  
+    return fighters[position[0]][position[1]];
+  });
+}
