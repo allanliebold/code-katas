6544a4f47fc8b6faf2b6f6ff35a95658bc01b77f
Allan Liebold <allan.liebold@gmail.com>
2018-06-02 19:15:15 -0700

    Update formatStringOfNames.js

--- javascript/formatStringOfNames.js
+++ javascript/formatStringOfNames.js
@@ -52,4 +52,19 @@ function list(names){
 }
 
 // Refactor
-
+function list(names){
+  var listStr = '';
+  
+  if(names.length > 0)
+    listStr += names[0].name;
+  
+  if(names.length > 1) {
+    for(var i = 1; i < names.length - 1; i++) {
+      listStr += ', ' + names[i].name;
+    }
+    
+    listStr += ' & ' + names[names.length - 1].name;
+  }
+  
+  return listStr;
+}
