3b8603ce008984649cd64ebbca3e7e1bb8622ca5
Allan Liebold <allan.liebold@gmail.com>
2018-06-02 17:08:57 -0700

    Update formatStringOfNames.js

--- javascript/formatStringOfNames.js
+++ javascript/formatStringOfNames.js
@@ -1,3 +1,22 @@
+/* Format a String of Names - 
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+Example:
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
+
+list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// returns 'Bart & Lisa'
+
+list([ {name: 'Bart'} ])
+// returns 'Bart'
+
+list([])
+// returns ''
+
+*/
+
 
 function list(names){
   var listStr = '';
